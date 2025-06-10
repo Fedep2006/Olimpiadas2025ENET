042cf412a1c134e5649d3a137fb65598a5eaccfe
aaronPro <aaroncascino@gmail.com>
2025-06-10 13:04:02 -0300

    usuario

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/', function () {
     return view('index');
 });
 Route::get('/login', function () {
+    createUserIfNotExists();
     return view('login.login');
 });
 Route::get('/register', function () {
